2faed6657696886e0584b099e8f2a6002b5cb93f
Philip Blunk <philipblunk@gmail.com>
2023-10-12 17:35:46 -0500

    Person and Search Screens

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import MovieScreen from '../screens/MovieScreen';
+import PersonScreen from '../screens/PersonScreen';
+import SearchScreen from '../screens/SearchScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +20,16 @@ export default function AppNavigation() {
           name="Movie"
           options={{ headerShown: false }}
           component={MovieScreen}
+        />
+        <Stack.Screen
+          name="Person"
+          options={{ headerShown: false }}
+          component={PersonScreen}
+        />
+        <Stack.Screen
+          name="Search"
+          options={{ headerShown: false }}
+          component={SearchScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>

24c4b02ece58b3cdf146bdbf5ab2e063612a3eb2
Philip Blunk <philipblunk@gmail.com>
2023-10-15 09:57:49 -0500

    Person Details API

--- screens/PersonScreen.js
+++ screens/PersonScreen.js
@@ -4,27 +4,58 @@ import {
   Dimensions,
   Platform,
   ScrollView,
-  SafeAreaView,
   TouchableOpacity,
   Image,
 } from 'react-native';
 import { ChevronLeftIcon } from 'react-native-heroicons/outline';
 import { HeartIcon } from 'react-native-heroicons/solid';
-import React, { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useEffect, useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { styles } from '../theme';
 import MovieList from '../components/movieList';
 import Loading from '../components/loading';
+import {
+  fallbackPersonImage,
+  fetchPersonDetails,
+  fetchPersonMovies,
+  image342,
+  image185,
+  image500,
+} from '../api/moviedb';
 
-var { width, height } = Dimensions.get('window');
 const ios = Platform.OS == 'ios';
 const verticalMargin = ios ? '' : ' my-3';
+var { width, height } = Dimensions.get('window');
 
 export default function PersonScreen() {
-  const navigation = useNavigation();
+  const { params: item } = useRoute();
   const [isFavorite, toggleFavorite] = useState(false);
-  const [personMovies, setPersonMovies] = useState([1, 2, 3, 4]);
+  const navigation = useNavigation();
+  const [person, setPerson] = useState([]);
+  const [personMovies, setPersonMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    //console.log('person: ', item);
+    getPersonDetails(item.id);
+    getPersonMovies(item.id);
+  }, [item]);
+
+  const getPersonDetails = async (id) => {
+    const data = await fetchPersonDetails(id);
+    // console.log('get person details: ', data);
+    if (data) setPerson(data);
+    setLoading(false);
+  };
+
+  const getPersonMovies = async (id) => {
+    const data = await fetchPersonMovies(id);
+    // console.log('get person movies: ', data);
+    if (data && data.cast) setPersonMovies(data.cast);
+  };
+
   return (
     <ScrollView
       className="flex-1 bg-neutral-900"
@@ -65,41 +96,52 @@ export default function PersonScreen() {
           >
             <View className="item-center rounded-full overflow-hidden h-72 w-72 border-2 border-neutral-500">
               <Image
-                source={require('../assets/images/castImage2.png')}
+                // source={require('../assets/images/castImage2.png')}
+                source={{
+                  uri: image342(person?.profile_path) || fallbackPersonImage,
+                }}
                 style={{ height: height * 0.43, width: width * 0.74 }}
               />
             </View>
           </View>
           <View className="mt-6">
             <Text className="text-3xl text-white font-bold text-center">
-              Keanu Reeves
+              {person?.name}
             </Text>
             <Text className="text-base text-neutral-500 text-center">
-              London, United Kingdom
+              {person?.place_of_birth}
             </Text>
           </View>
           <View className="mx-3 p-4 mt-6 flex-row justify-between items-center bg-neutral-700 rounded-full">
             <View className="border-r-2 border-r-neutral-400 px-2 items-center">
               <Text className="text-white font-semibold">Gender</Text>
-              <Text className="text-neutral-300 text-sm">Male</Text>
+              <Text className="text-neutral-300 text-sm">
+                {person?.gender == 1 ? 'Female' : 'Male'}
+              </Text>
             </View>
             <View className="border-r-2 border-r-neutral-400 px-2 items-center">
               <Text className="text-white font-semibold">Birthday</Text>
-              <Text className="text-neutral-300 text-sm">1964-09-02</Text>
+              <Text className="text-neutral-300 text-sm">
+                {person?.birthday}
+              </Text>
             </View>
             <View className="border-r-2 border-r-neutral-400 px-2 items-center">
               <Text className="text-white font-semibold">Known for</Text>
-              <Text className="text-neutral-300 text-sm">Acting</Text>
+              <Text className="text-neutral-300 text-sm">
+                {person?.known_for_department}
+              </Text>
             </View>
             <View className="px-2 items-center">
               <Text className="text-white font-semibold">Popularity</Text>
-              <Text className="text-neutral-300 text-sm">64.23</Text>
+              <Text className="text-neutral-300 text-sm">
+                {person?.popularity?.toFixed(2)} %
+              </Text>
             </View>
           </View>
           <View className="my-6 mx-4 space-y-2">
             <Text className="text-white text-lg">Biography</Text>
             <Text className="text-neutral-400 tracking-wide">
-              Input Biography Here.
+              {person?.biography || 'N/A'}
             </Text>
           </View>
 
